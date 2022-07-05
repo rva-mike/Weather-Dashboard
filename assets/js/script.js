8c85740261ce4a17de503adc8bb1759974a80ace
Mike <mike.levy.rva@gmail.com>
2022-07-05 16:37:53 -0400

    added 5 day weather forecast and date to current weather

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,9 +6,15 @@ var tempEl = document.querySelector(".temp");
 var windEl = document.querySelector(".wind")
 var humidityEl = document.querySelector(".humidity")
 var uvIndexEl = document.querySelector(".uvIndex")
-
+var dayOne5 = document.querySelector(".dayOne")
+var dayTwo5 = document.querySelector(".dayTwo")
+var dayThree5 = document.querySelector(".dayThree")
+var dayFour5 = document.querySelector(".dayFour")
+var dayFive5 = document.querySelector(".dayFive")
+var todayDate = document.querySelector(".currentDay")
 
 var tempEl1 = document.querySelector("temp-1")
+
 
 
 searchButtonEl.addEventListener("click", function () {
@@ -17,17 +23,70 @@ searchButtonEl.addEventListener("click", function () {
         .then(data => {
             console.log(data)
             var nameValue = data["name"];
-            var tempValue = "Temperature " + data['main']["temp"] + "&#176F";
-            var windValue = "Wind " + data.wind.speed + " MPH";
-            var humidityValue = "Humidity: " + data["main"]['humidity'] + "%";
-            var uvIndexValue = ["uvi"];
+            var currentDate = moment().format("dddd, MMMM Do");
+            var tempValue = "<b>Temperature: </b>" + data['main']["temp"] + "&#176F";
+            var windValue = "<b>Wind: </b>" + data.wind.speed + " MPH";
+            var humidityValue = "<b>Humidity: </b>" + data["main"]['humidity'] + "%";
+            var uvIndexValue = ["<b>UV Index: </b>"];
 
             nameEl.innerHTML = nameValue;
             tempEl.innerHTML = tempValue;
             windEl.innerHTML = windValue;
             humidityEl.innerHTML = humidityValue;
             uvIndexEl.innerHTML = uvIndexValue;
+            todayDate.innerHTML = currentDate;
+
+
 
 
         })
 })
+
+
+
+// five-day api call
+searchButtonEl.addEventListener("click", function () {
+    fetch("https://api.openweathermap.org/data/2.5/forecast?q=" + inputValueEl.value + '&appid=529b41dd58ddfd8a41cab95232af43ae&units=imperial')
+        .then(response => response.json())
+        .then(data => {
+            console.log(data)
+            var firstDay = "<b>Temp: </b>" + data['list'][1]["main"]["temp"] + "&#176F. " + "<br>" + "<b>Wind: </b>" + data["list"][1]["wind"]["speed"] + " MPH" + "<br>" + "<b>Humidity: </b>" + data["list"][1]["main"]["humidity"] + "%";
+
+            dayOne5.innerHTML = firstDay;
+
+
+            var secondDay = "<b>Temp: </b>" + data['list'][2]["main"]["temp"] + "&#176F. " + "<br>" + "<b>Wind: </b>" + data["list"][2]["wind"]["speed"] + " MPH" + "<br>" + "<b>Humidity: </b>" + data["list"][2]["main"]["humidity"] + "%";
+
+            dayTwo5.innerHTML = secondDay;
+
+            var thirdDay = "<b>Temp: </b>" + data['list'][3]["main"]["temp"] + "&#176F. " + "<br>" + "<b>Wind: </b>" + data["list"][3]["wind"]["speed"] + " MPH" + "<br>" + "<b>Humidity: </b>" + data["list"][3]["main"]["humidity"] + "%";
+
+            dayThree5.innerHTML = thirdDay;
+
+            var fourthDay = "<b>Temp: </b>" + data['list'][4]["main"]["temp"] + "&#176F. " + "<br>" + "<b>Wind: </b>" + data["list"][4]["wind"]["speed"] + " MPH" + "<br>" + "<b>Humidity: </b>" + data["list"][4]["main"]["humidity"] + "%";
+
+            dayFour5.innerHTML = fourthDay;
+
+            var fifthDay = "<b>Temp: </b>" + data['list'][5]["main"]["temp"] + "&#176F. " + "<br>" + "<b>Wind: </b>" + data["list"][5]["wind"]["speed"] + " MPH" + "<br>" + "<b>Humidity: </b>" + data["list"][5]["main"]["humidity"] + "%";
+
+            dayFive5.innerHTML = fifthDay;
+
+
+
+            // var firstDayTime = data["list"][1]["dt_txt"]
+
+
+
+
+
+
+
+
+        })
+})
+
+
+
+
+// var currentDate = moment().format("dddd, MMMM Do")
+//             $("#currentDay").text(currentDate)
